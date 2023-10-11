7944115928ed960302534a3abd9720b7a9b3ac70
newExpand <magin0auto@gmail.com>
2023-10-11 20:44:05 +0900

    :sparkles:feat: env파일을 활용하여 mongoDB 및 프론트 Cors 설정 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user/user.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
+
+import { ENV_FILE_PATH } from './constant/path';
 
 @Module({
-    imports: [UserModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: ENV_FILE_PATH,
+        }),
+        MongooseModule.forRoot(process.env.MONGO_URI),
+        UserModule,
+    ],
     controllers: [],
     providers: [UserService],
 })
---
+++ src/constant/path.ts
@@ -0,0 +1,1 @@
+export const ENV_FILE_PATH = `${process.cwd()}/envs/${process.env.NODE_ENV}.env`.replace(/\s/g, '');
