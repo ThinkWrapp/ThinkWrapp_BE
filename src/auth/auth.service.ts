c9b0aa0272cae303064136442480b3253ca15790
newExpand <magin0auto@gmail.com>
2023-11-15 10:21:06 +0900

    :bug:fix: 쿠키가 전달되지 않아 sameSite: none 옵션 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -129,7 +129,7 @@ export class AuthService {
         res.cookie('refresh', refresh_token, {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 7,
-            sameSite: 'lax',
+            sameSite: process.env.NODE_ENV === 'dev' ? 'lax' : 'none',
             secure: process.env.NODE_ENV === 'dev' ? false : true,
         });
 
